5ffbe5f3e44bff7c74c27345d7ac2d0ebbbd7a6b
Broch Stilley <Broch Stilley>
2016-07-04 17:50:16 -0700

    Adding boxes. Generally getting things working.

--- src/index.js
+++ src/index.js
@@ -1,29 +1,68 @@
 import Babylon from 'babylonjs';
 
+function createBoxes(scene) {
+  /* Boxes */
+  let box1 = new Babylon.Mesh.CreateBox('box1', 0.5, scene);
+  box1.position = new Babylon.Vector3(0, 0, -3);
+  box1.material = new Babylon.StandardMaterial('texture1', scene);
+  box1.material.backFaceCulling = false;
+  box1.material.diffuseColor = new Babylon.Color3.Red();
+
+  let box2 = new Babylon.Mesh.CreateBox('box2', 0.5, scene);
+  box2.position = new Babylon.Vector3(3, 1, 0);
+  box2.material = new Babylon.StandardMaterial('texture2', scene);
+  box2.material.backFaceCulling = false;
+  box2.material.diffuseColor = new Babylon.Color3.Green();
+
+  let box3 = new Babylon.Mesh.CreateBox('box3', 0.5, scene);
+  box3.position = new Babylon.Vector3(0, 1, 3);
+  box3.material = new Babylon.StandardMaterial('texture3', scene);
+  box3.material.backFaceCulling = false;
+  box3.material.diffuseColor = new Babylon.Color3.Blue();
+
+  let box4 = new Babylon.Mesh.CreateBox('box4', 0.5, scene);
+  box4.position = new Babylon.Vector3(-3, 1, 0);
+  box4.material = new Babylon.StandardMaterial('texture4', scene);
+  box4.material.backFaceCulling = false;
+  box4.material.diffuseColor = new Babylon.Color3.Purple();
+
+  let box5 = new Babylon.Mesh.CreateBox('box5', 0.5, scene);
+  box5.position = new Babylon.Vector3(0, 1, 0);
+  box5.material = new Babylon.StandardMaterial('texture6', scene);
+  box5.material.backFaceCulling = false;
+  box5.material.diffuseColor = new Babylon.Color3.White();
+}
+
 window.addEventListener("DOMContentLoaded", () => {
     const canvas = document.getElementById('render-canvas');
-    console.log('got a canvas!', canvas);
 
     let engine = new Babylon.Engine(canvas, true);
 
     let scene = new Babylon.Scene(engine);
 
-    let camera = new Babylon.FreeCamera('camera1', new Babylon.Vector3(0, 5,-10), scene);
+    navigator.getVRDisplays().then((displays) => {
+        let display = displays[0];
 
-    camera.setTarget(Babylon.Vector3.Zero());
+        let camera = new Babylon.VRRoomScaleCamera('camera2', new Babylon.Vector3(0, 0, 0), display, scene);
+        camera.setTarget(Babylon.Vector3.Zero());
+        camera.attachControl(canvas, false, 'something here.');
 
-    camera.attachControl(canvas, false);
+        let light = new Babylon.HemisphericLight('light1', new Babylon.Vector3(0,1,0), scene);
 
-    let light = new Babylon.HemisphericLight('light1', new Babylon.Vector3(0,1,0), scene);
+        const {sizeX, sizeZ} = display.stageParameters;
+        console.log("room size", sizeX, sizeZ);
+        let ground = Babylon.Mesh.CreateGround('ground1', sizeX, sizeZ, 2, scene);
+        ground.material = new Babylon.StandardMaterial('groundTexture1', scene);
+        ground.material.backFaceCulling = false;
 
-    let sphere = Babylon.Mesh.CreateSphere('sphere1', 16, 2, scene);
+        createBoxes(scene);
 
-    sphere.position.y = 1;
+        function onAnimationFrame(){
+          display.requestAnimationFrame(onAnimationFrame);
+          scene.render();
+        }
 
-    let ground = Babylon.Mesh.CreateGround('ground1', 6, 6, 2, scene);
-
-    engine.runRenderLoop(() => {
-        scene.render();
+        display.requestAnimationFrame(onAnimationFrame);
     });
 
     // the canvas/window resize event handler
